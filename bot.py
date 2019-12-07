9960f4786bfcd3971543c77dcba5f5ecab751a03
imConnor <54060984+imConnorrr@users.noreply.github.com>
2019-12-07 01:53:20 -0500

    Update and rename quizizz-bot.py to bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-#-------------------------------------------------------------------------#
 global enabled
 def randdelaygen(base,tf):
     if tf==True:
@@ -48,7 +47,12 @@ def play(gamecode, name, randdelay):
     # enable browser logging
     d = DesiredCapabilities.CHROME
     d['loggingPrefs'] = { 'performance':'ALL' }
-    driver = webdriver.Chrome(desired_capabilities=d)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     
     driver.get("https://quizizz.com/join/")
     print("[info] Starting game")
