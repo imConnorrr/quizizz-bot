2be8472ba96205a5cab07270d710f0f940c9ba8e
imConnor <54060984+imConnorrr@users.noreply.github.com>
2019-12-11 20:30:55 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,24 @@
+#from pyvirtualdisplay import Display
+import subprocess
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import random
+import sys
+import time
+import os
+
+gamepin = sys.argv[1]
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1024,728")
+chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
@@ -7,8 +28,6 @@ inputElement = driver.find_element_by_class_name("check-room-input")
 inputElement.send_keys(gamepin)
 inputElement.send_keys(Keys.ENTER)
 time.sleep(1)
-driver.get('https://kahoothack.org/')
-time.sleep(12)
 #    invalidpinbox = driver.find_element_by_class_name("content right error")
 
 invalidcode = driver.find_elements_by_xpath("//div[@class='content right error']")
